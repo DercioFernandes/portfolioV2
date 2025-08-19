1627338cb4f9d4cefdcca2611357b12de8d73d05
DercioFernandes <315236@via.dk>
2025-08-19 09:53:38 +0100

    Updates to Links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <p className="text-lg mb-4">© 2025 Dércio Fernandes</p>
         <div className="flex justify-center space-x-6 text-2xl">
           <a
-            href="https://instagram.com/yourusername"
+            href="https://www.instagram.com/dercio_fernandes/"
             target="_blank"
             rel="noopener noreferrer"
             className="text-pink-500 hover:text-pink-400"
@@ -23,7 +23,7 @@ export default function Footer() {
             <FaEnvelope />
           </a>
           <a
-            href="https://github.com/yourusername"
+            href="https://github.com/DercioFernandes"
             target="_blank"
             rel="noopener noreferrer"
             className="text-gray-100 hover:text-gray-400"
@@ -31,7 +31,7 @@ export default function Footer() {
             <FaGithub />
           </a>
           <a
-            href="https://facebook.com/yourusername"
+            href="https://www.facebook.com/dercio.fernandes.96/"
             target="_blank"
             rel="noopener noreferrer"
             className="text-blue-600 hover:text-blue-400"
